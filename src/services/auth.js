ef675cb0b8a20e6b6717928c7b8cacebe26edbe3
Michelle Ross <miross629@gmail.com>
2023-02-13 11:38:49 -0800

    Add auth services for authUser()

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,22 @@
-import { client } from "./client.js";
+import { client } from './client.js';
 
 // get current user from Supabase
 export function getUser() {
   return client.auth.currentUser;
 }
+
+// get email, password, and type
+export async function authUser(email, password, type) {
+  let response;
+  if (type === 'sign-up') {
+    response = await client.auth.signUp({ email, password });
+  } else {
+    response = await client.auth.signIn({ email, password });
+  }
+  if (response.error) {
+    throw response.error;
+  }
+  return response.user;
+}
+
+// TODO sign out user
