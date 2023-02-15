0ef03ba6a3a8132961ef5565afcc60d3eb24d5cd
Michelle Ross <miross629@gmail.com>
2023-02-15 09:44:35 -0800

    Create separate sign in and sign up forms in Auth.js, TODO: onChange

--- src/components/Main/Auth/Auth.js
+++ src/components/Main/Auth/Auth.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
-import { NavLink, Redirect, useParams } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { useUser } from '../../../context/UserContext.js';
-import { authUser } from '../../../services/auth.js';
+import { signInUser, signUpUser } from '../../../services/auth.js';
 
 export default function Auth() {
   // initialize state
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const { type } = useParams();
   const { user, setUser } = useUser();
 
-  // TODO: send users to Inventory/
+  // send users to Inventory/
   if (user) {
     return <Redirect to="/inventory" />;
   }
 
-  // form submit function to send info to Supabase
-  const submitAuth = async (e) => {
+  // event listener for sign in form
+  const submitSignIn = async (e) => {
     e.preventDefault();
     try {
-      const newUser = await authUser(email, password, type);
+      const newUser = await signInUser(email, password);
+      setUser(newUser);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // event listener for sign up form
+  const submitSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const newUser = await signUpUser(email, password);
       setUser(newUser);
     } catch (e) {
       console.error(e);
@@ -31,31 +40,51 @@ export default function Auth() {
     <div>
       <h2>Welcome!</h2>
 
-      <nav>
-        {/* sign in */}
-        <NavLink to="/auth/sign-in">Sign in</NavLink>
-        {/* sign up */}
-        <NavLink to="/auth/sign-up">Sign up</NavLink>
-      </nav>
+      {/* form: sign in */}
+      <section>
+        <h3>Sign In</h3>
+        <form onSubmit={submitSignIn}>
+          {/* email fieldset */}
+          <fieldset>
+            <label>Email</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+          </fieldset>
+          {/* password fieldset */}
+          <fieldset>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </fieldset>
+          {/* button */}
+          <button type="submit">Submit</button>
+        </form>
+      </section>
 
-      <form onSubmit={submitAuth}>
-        {/* email fieldset */}
-        <fieldset>
-          <label>Email</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-        </fieldset>
-        {/* password fieldset */}
-        <fieldset>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-        </fieldset>
-        {/* button */}
-        <button type="submit">Submit</button>
-      </form>
+      {/* form: sign up */}
+      <section>
+        <h3>Sign Up</h3>
+        <form onSubmit={submitSignUp}>
+          {/* email fieldset */}
+          <fieldset>
+            <label>Email</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+          </fieldset>
+          {/* password fieldset */}
+          <fieldset>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </fieldset>
+          {/* button */}
+          <button type="submit">Submit</button>
+        </form>
+      </section>
     </div>
   );
 }
