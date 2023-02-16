6e0d3955fdef945c92bf4118d0e3d1c2d92ccf00
Michelle Ross <miross629@gmail.com>
2023-02-15 17:38:30 -0800

    Send stocked value of true or false to Supabase

--- src/components/Main/Inventory/AddInventory/AddInventory.js
+++ src/components/Main/Inventory/AddInventory/AddInventory.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { InventoryContext } from '../../../../context/InventoryContext.js';
+import { createItemRow } from '../../../../services/inventory.js';
 
 export default function AddInventory() {
+  // initialize state for name and stocked
+  const [itemName, setItemName] = useState('');
+  const [stocked, setStocked] = useState(true);
+
+  // call setInventory from InventoryContext
+  const { setInventory } = useContext(InventoryContext);
+
+  // function to send user item input to Supabase
+  const handleNewItem = async () => {
+    try {
+      const itemRow = await createItemRow(itemName, stocked);
+      setInventory((prev) => [...prev, itemRow]);
+      setItemName('');
+      setStocked(true);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
   return (
     <div>
       <h2>Add Item to Inventory</h2>
@@ -8,24 +29,44 @@ export default function AddInventory() {
       <form>
         {/* input: name */}
         <div>
-          <label>Name of item</label>
-          <input type="text" />
+          <label htmlFor="name">Name of item</label>
+          <input
+            type="text"
+            name="name"
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+          />
         </div>
 
         {/* radio buttons: stocked, low stock */}
+        {/* TODO send stocked value to Supabase */}
         <fieldset>
           <legend>Current stock</legend>
           {/* stocked */}
-          <input type="radio" />
-          <label>Stocked</label>
+          <input
+            type="radio"
+            name="current-stock"
+            id="stocked"
+            value={true}
+            onChange={(e) => setStocked(e.target.value)}
+          />
+          <label htmlFor="stocked">Stocked</label>
           {/* low stock */}
-          <input type="radio" />
-          <lable>Low stock</lable>
+          <input
+            type="radio"
+            name="current-stock"
+            id="low-stock"
+            value={false}
+            onChange={(e) => setStocked(e.target.value)}
+          />
+          <label htmlFor="low stock">Low stock</label>
         </fieldset>
 
         {/* button: add item */}
         <div>
-          <button type="submit">Add Item</button>
+          <button type="submit" onClick={handleNewItem}>
+            Add Item
+          </button>
         </div>
       </form>
     </div>
