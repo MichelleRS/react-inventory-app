57298b58f6cb82a3cd0befc50a78c62819566c63
Michelle Ross <miross629@gmail.com>
2023-02-17 13:17:05 -0800

    Add comment TODOs

--- src/components/Main/Inventory/EditInventory/ItemCard.js
+++ src/components/Main/Inventory/EditInventory/ItemCard.js
@@ -4,16 +4,22 @@ import { InventoryContext } from '../../../../context/InventoryContext.js';
 export default function ItemCard() {
   const { inventory } = useContext(InventoryContext);
 
+  // TODO: function to change stock value of item
+
+  // TODO: function to delete item from inventory
+
   return (
     <div>
-      {/* begin rendering an item card using .map */}
+      {/* TODO: remove div wrapper, move .map to EditInventory for each based on stock value */}
       <>
         {inventory.map((item) => (
           <ul key={item.id}>
             <li>
               <h2>{item.name}</h2>
               {/* TODO: button to change stock value */}
-              <button type="button">{String(item.stocked)}</button>
+              <button type="button" value={item.stocked} name="current-stock">
+                {String(item.stocked)}
+              </button>
               {/* TODO: button to delete item*/}
               <button type="button">Delete Item</button>
             </li>
