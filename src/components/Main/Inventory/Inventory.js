626d393e63ed2f5b30728d35269503d2c7fad804
Michelle Ross <miross629@gmail.com>
2023-02-17 11:44:49 -0800

    Display current item names, TODO: display by stocked value

--- src/components/Main/Inventory/Inventory.js
+++ src/components/Main/Inventory/Inventory.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useUser } from '../../../context/UserContext.js';
 import AddInventory from './AddInventory/AddInventory.js';
+import EditInventory from './EditInventory/EditInventory.js';
 
 export default function Inventory() {
   // redirect to auth if not a user
@@ -13,8 +14,7 @@ export default function Inventory() {
   return (
     <main>
       <AddInventory />
-      <h2>Edit Inventory</h2>
-      {/* TODO: add path to EditInventory/ */}
+      <EditInventory />
     </main>
   );
 }
